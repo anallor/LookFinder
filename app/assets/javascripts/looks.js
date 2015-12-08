628f30e7c3b16b8deeecb07cd48991da69ac9c49
Ana Llorca <ana.llorca.a@gmail.com>
2015-12-08 16:48:34 +0100

    added some jquery features for page of looks

--- app/assets/javascripts/looks.js
+++ app/assets/javascripts/looks.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$( document ).ready(function() {
+
+$('.ideas3').hover(function(){
+   $('.three').html('LBD + Perfecto de cuero negra');
+});
+
+$('.ideas2').hover(function(){
+   $('.two').html('Falda de tul rosa');
+});
+
+$('.ideas1').hover(function(){
+   $('.one').html('Falda de tul negra');
+});
+
+});
+
+
+
+
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,7 +5,8 @@ class RequestsController < ApplicationController
 	end
 
 	def create
-		@user = User.last
+	 if user_signed_in?
+		@user = User.find(current_user)
 		@request = @user.requests.new request_params
 
 		@stores_by_place = Store.where(Store.arel_table[:place].matches(@request.place))
@@ -19,6 +20,22 @@ class RequestsController < ApplicationController
 		else
 			render 'new'
 		end
+
+	 else
+		@request = Request.new request_params
+
+		@stores_by_place = Store.where(Store.arel_table[:place].matches(@request.place))
+		 s = @stores_by_place
+		 @store_emails = s.map { |store| store.email }
+
+		if @request.save
+			RequestMailer.request_email(@store_emails).deliver_now
+			flash[:notice] = 'Request created successfully'
+			redirect_to request_path(@request.id)
+		else
+			render 'new'
+		end
+	 end
 	end
 
 	def show
